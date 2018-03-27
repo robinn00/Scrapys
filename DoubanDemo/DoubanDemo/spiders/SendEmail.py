d397b4a94f5ec7939896c7da5ba24ed1bffa5f2e
robinn00 <robinn00@163.com>
2018-03-27 13:48:24 +0800

    scrapy

--- DoubanDemo/DoubanDemo/spiders/SendEmail.py
+++ DoubanDemo/DoubanDemo/spiders/SendEmail.py
@@ -5,5 +5,5 @@
 from scrapy.mail import MailSender
 
 if __name__ == "__main__":
-    ms = MailSender(smtphost="smtp.163.com",mailfrom="ozivizo@163.com",smtpuser="ozivizo@163.com",smtppass="doxdox45600",smtpport=25,smtptls=True)
-    ms.send(to='3110056662@qq.com',subject='testhe',body='ssssssssssss',cc='1641213130@qq.com',mimetype="text/plain")
+    ms = MailSender(smtphost="smtp.163.com",mailfrom="@163.com",smtpuser="@163.com",smtppass="",smtpport=25,smtptls=True)
+    ms.send(to='@qq.com',subject='testhe',body='ssssssssssss',cc='@qq.com',mimetype="text/plain")
